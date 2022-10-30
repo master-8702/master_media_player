c0b8fcc4c63946e2016aa107ce39507302003593
master-8702 <55705389+master-8702@users.noreply.github.com>
2022-10-30 21:09:11 +0300

    initial commit

---
+++ lib/screens/song_playing_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SongPlayingPage extends StatefulWidget {
+  const SongPlayingPage({Key? key}) : super(key: key);
+
+  @override
+  State<SongPlayingPage> createState() => _SongPlayingPageState();
+}
+
+class _SongPlayingPageState extends State<SongPlayingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
